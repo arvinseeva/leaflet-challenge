a382afead13de0a9a7d484dbd19104bb6b6c395a
arvinseeva <65349222+arvinseeva@users.noreply.github.com>
2020-10-02 20:52:56 +0800

    Update logic.js

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,10 @@
+var quakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+var plates = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json"
+
+var color0 = "limegreen";
+var color1 = "chartreuse";
+var color2 = "yellow";
+var color3 = "orange";
+var color4 = "darkorange";
+var color5 = "red";
+var legend = L.control();
